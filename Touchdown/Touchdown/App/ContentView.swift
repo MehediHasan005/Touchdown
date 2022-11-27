2972ed5548647effb977683f993d4f4cdc549ace
MehediHasan005 <mehedihasanhira85@gmail.com>
2022-11-27 14:34:22 +0600

    slider view added

--- Touchdown/Touchdown/App/ContentView.swift
+++ Touchdown/Touchdown/App/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack(spacing:0) {
+                NavigationBarView()
+                    .padding(.horizontal , 15)
+                    .padding(.bottom)
+                    .padding(.top , UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color:.black .opacity(0.05),radius: 5,x: 0,y: 5)
+                ScrollView(.vertical , showsIndicators: false) {
+                    VStack(spacing: 0) {
+                        FeaturedTabView()
+                            .frame(height: 300)
+                            
+                            //.padding(.vertical ,20)
+                        FooterView()
+                    }
+                }
+                
+            }
+            .background(colorBackground .ignoresSafeArea(.all , edges: .all))
+        }
+        .ignoresSafeArea(.all , edges: .all)
+                         
     }
 }
 
