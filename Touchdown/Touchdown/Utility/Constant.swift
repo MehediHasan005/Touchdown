4952f82a9f17802466917d97c2cf7b19dbbe31a3
MehediHasan005 <mehedihasanhira85@gmail.com>
2022-11-27 12:53:50 +0600

    footer section added

--- Touchdown/Touchdown/Utility/Constant.swift
+++ Touchdown/Touchdown/Utility/Constant.swift
@@ -5,4 +5,7 @@
 //  Created by Mehedi Hasan on 27/11/22.
 //
 
-import swiftui
+import SwiftUI
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
